9466e516d45ddda6c9c3307c576b5aeda65fdbeb
Aoi Tamiuchi <mknk231714@gmail.com>
2026-07-29 20:10:05 -0700

    stg_customer cleaning done

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,7 +1,7 @@
 SELECT
-    customer_id,
-    customer_unique_id,
-    customer_zip_code_prefix,
-    customer_city,
-    customer_state
+customer_id, --per order
+customer_unique_id, -- per customer
+customer_zip_code_prefix as zip_code,
+TRIM(customer_city) as city,
+TRIM(customer_state) as states
 FROM raw.customers
